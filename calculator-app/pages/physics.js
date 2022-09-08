9afd89f3797c9be52ae1bc1920a1272aac546fd2
Maxim <albinauric21@gmail.com>
2022-09-08 13:54:29 +0100

    Objects with numbers added

--- calculator-app/pages/physics.js
+++ calculator-app/pages/physics.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useRef } from 'react';
 import Matter from 'matter-js';
 import create from "zustand"
+import { FaBlackTie } from 'react-icons/fa';
 
 const numbersToSpawn = [];
 
@@ -32,21 +33,52 @@ export const PhysicsMap = () => {
       }
     });
 
+    const spawnInfo = {
+        width: window.innerWidth / 2,
+        height: 100,
+        radius: 40
+    }
+
+    const {width, height, radius} = spawnInfo;
+
     const calculatorBody = Bodies.rectangle(x, y, 340, 573, {
       isStatic: true,
+      chamfer: 16,
       render: {
         fillStyle: 'none'
       }
     });
 
-    const ball = Bodies.circle(x, 45, 10, {
-      restitution: 0.9,
+    const floor = Bodies.rectangle(x, y*2, x*2, 10, {
+        isStatic: true,
+        render: {
+          fillStyle: 'none'
+        }
+      });
+
+    const ball = Bodies.circle(width, height, radius, {
       render: {
-        fillStyle: 'yellow'
+        sprite: {
+            texture: createImage("1"),
+        }
       }
     });
 
-    World.add(engine.world, [calculatorBody, ball]);
+
+    // const wordBody = Bodies.rectangle(x, 50, 25, 25, {
+    //     restitution: 0.95,
+    //     friction: 0,
+    //     render: {
+    //      fillStyle: "#FFFFFF",
+    //      text: {
+    //       fillStyle: "#000000",
+    //       content: "1",
+    //       size: 50,
+    //      },
+    //    },
+    //   });
+
+    World.add(engine.world, [calculatorBody, ball, floor]);
 
     Engine.run(engine);
     Render.run(render);
@@ -62,3 +94,27 @@ export const PhysicsMap = () => {
     </div>
   );
 };
+
+function createImage(string) {
+
+    let drawing = document.createElement("canvas");
+
+    drawing.width = '150'
+    drawing.height = '150'
+
+    let ctx = drawing.getContext("2d");
+
+    ctx.fillStyle = "none";
+    //ctx.fillRect(0, 0, 150, 150);
+    ctx.beginPath();
+    ctx.arc(75, 75, 40, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+    ctx.fillStyle = "white";
+    ctx.font = "25pt sans-serif";
+    ctx.textAlign = "center";
+    ctx.fillText(string, 75, 85);
+    // ctx.strokeText("Canvas Rocks!", 5, 130);
+
+    return drawing.toDataURL("image/png");
+}
